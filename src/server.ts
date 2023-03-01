3c72d89e10eef9affd83c6d7fbcc14a25aea15f7
Ferreira2912 <107006102+Ferreira2912@users.noreply.github.com>
2023-02-28 21:07:05 -0300

    Update server.ts

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express  from "express";
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (request, response) => {
+  response.send('Hello World!')
+});
+
+app.listen(port, () => {
+  console.log("Server Running!! 👍");
+
+})
+
